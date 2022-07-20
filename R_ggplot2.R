5bf1fe57e53a307952847d7c5ac0ac1004e4b7d9
Hakim-Adjedjou <60810965+Hakim-Adjedjou@users.noreply.github.com>
2022-07-20 10:23:13 +0100

    modify script

--- R_ggplot2.R
+++ R_ggplot2.R
@@ -27,13 +27,15 @@ df$Pclass<-as.factor(df$Pclass)
 df$Sex<-as.factor(df$Sex)
 df$Embarked<-as.factor(df$Embarked)
 
-#question 1 : what s the distribution of survival rates in general ?
+#question 1 : what s the distribution of survival rates in general (by %) ?
 
-ggplot(df , aes(x=Survived))+theme_bw()+
-  geom_bar()+labs(y="number of passengers",title = "survival rates")
+ggplot(df , aes(x=Survived, y=after_stat(100*count/sum(count))))+theme_bw()+
+  geom_bar()+labs(y="number of passengers by %",title = "survival rates")
 
 # in-depth ( showing propotions of survival) : 
 prop.table(table(df$Survived))
+
+#notice thta after_stat is used to get the computed count (statistics layer)
 
 
 #question 2 : how much survived per sex ?
@@ -59,5 +61,19 @@ ggplot(df , aes(x=Sex , fill=Survived))+
 #class3
 
 
+#question 4 : doing a 4 dimensionnal visualisation ( survival rates per (age+sex+class))
+ggplot(df , aes(x=Age , fill=Survived))+
+  theme_bw()+
+  geom_density(alpha=0.5)+
+  facet_wrap(Sex~Pclass)+
+  labs(y="number of passengers",title = "survival rates by Age")
 
 
+
+#question 5 : show the distribution of Age by pclass with mean and median on it :
+ggplot(df)+
+  theme_bw()+
+  geom_jitter(aes(x=Pclass, y=Age))+
+  stat_summary(aes(x=Pclass,y=Age),fun = mean ,geom = "point", colour="red")+
+  stat_summary(aes(x=Pclass,y=Age),fun = median ,geom = "point", colour="yellow")+
+  labs(x="class type",title = "age distribution by class")
